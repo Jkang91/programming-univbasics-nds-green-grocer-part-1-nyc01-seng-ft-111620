fb4574a3e9973c2b60de070ef406e3a7c1befb04
June Young Kang <jkang34@gmail.com>
2020-10-29 18:13:23 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,11 +2,9 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    item.each do |key, value|   #item.key
-      if value == name
-        return key => value
-      end
-    end  
+    if item.value == name
+      return key => value
+    end
   end
   return nil
 end

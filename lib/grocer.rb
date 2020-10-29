d3d38c134384637820ddeea8b0894940cae39b9d
June Young Kang <jkang34@gmail.com>
2020-10-29 18:01:23 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,7 +16,7 @@ def consolidate_cart(cart)
   counter = 0 
   while counter < cart.length 
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if new_cart_item != nil
+    if new_cart_item 
       new_cart_item[:count] += 1
     else 
       new_cart_item = {

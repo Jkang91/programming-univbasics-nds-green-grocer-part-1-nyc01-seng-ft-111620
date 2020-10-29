7de4ad17856dfc7554fe411fc09005682ff00edb
June Young Kang <jkang34@gmail.com>
2020-10-29 18:51:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -37,11 +37,4 @@ end
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
- # collection.each do |item|
-  #   if item.values.join == name
-  #     return item
-  #   end
-  # end
-  # return nil
-  # binding.pry
   

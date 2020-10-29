09d08815a13e2436fe22e234d306acb5ddc2109f
June Young Kang <jkang34@gmail.com>
2020-10-29 18:38:13 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,6 +20,8 @@ def consolidate_cart(cart)
     else 
       new_cart_item = {
         :item => cart[counter][:item],
+        :price => cart[counter][:price]
+        :clearance =>
         :count => 1 
       }
       new_cart << new_cart_item
